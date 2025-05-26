6ecb429e95954169dceeec133ac514bc0f1fdc70
Miguel Lopez <juanjoxdxdxdxdxdxdx@gmail.com>
2025-05-25 20:37:01 -0600

    Update MainActivity.java

--- app/src/main/java/com/example/aplicacion/MainActivity.java
+++ app/src/main/java/com/example/aplicacion/MainActivity.java
@@ -12,28 +12,24 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main); // Establece el layout de la pantalla principal
+        setContentView(R.layout.activity_main);
 
         Button btnGoToDrawingApp = findViewById(R.id.btnGoToDrawingApp);
         Button btnGoToCredits = findViewById(R.id.btnGoToCredits);
 
-        // Configura el botón para ir a la aplicación de dibujo
         btnGoToDrawingApp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Crea un Intent para iniciar DrawingAppActivity
                 Intent intent = new Intent(MainActivity.this, DrawingAppActivity.class);
-                startActivity(intent); // Inicia la nueva Activity
+                startActivity(intent);
             }
         });
 
-        // Configura el botón para ir a la vista de créditos
         btnGoToCredits.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Crea un Intent para iniciar CreditsActivity
                 Intent intent = new Intent(MainActivity.this, CreditsActivity.class);
-                startActivity(intent); // Inicia la nueva Activity
+                startActivity(intent);
             }
         });
     }
